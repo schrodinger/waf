10eaed8c04e3781b860b5feeacfec824cca345da
Tor Colvin <Tor.Colvin@schrodinger.com>
2018-04-07 21:59:41 -0400

    Waf patch for Fortran .mod dependencies
    This patch makes Fortran source nodes that USE a module depend only on
    the .mod node and not on the object file generated from the module
    source file. This makes it possible to reduce recompilation when the
    module source file changes in a trivial way that doesn't affect the
    contents of the .mod file.
    
    In other words, if foo.f has "USE mymod", and mymod is defined in
    mymod.f, we turn this dependency graph:
    
      foo.f ---> mymod.mod ---> mymod.f
       |                        ^
       |                        |
       \-------> mymod.o -------/
    
    Into this:
    
      foo.f ---> mymod.mod ---> mymod.f
                                ^
                                |
                 mymod.o -------/
    
    In practice, when using ifort, to get the benefit described above one
    also needs to do two more things:
    
    1) Decorate the fc class with Task.update_outputs. Not done as part of
    this patch to leave the decision up to the wscript author.
    
    2) Monkey-patch Utils.h_file() to strip the silly timestamp that ifort
    inserts in the .mod files it writes, but that's not included in this
    patch because it is an implementation-dependent hack (this patch fixes
    an actual bug IMHO). Note: even stripping the timestamp is not perfect
    because ifort sometimes inserts other mystery junk into the .mod file,
    but at least we can avoid unnecessary recompilation *some* of the time.
    
    If 1) is not done, this patch won't change the current behavior, which
    is to have foo.f effectively be dependent on mymod.f because mymod.mod
    inherits the signature from mymod.f.

--- waflib/Tools/fc.py
+++ waflib/Tools/fc.py
@@ -47,6 +47,10 @@ def modfile(conf, name):
 		'UPPER.mod' :modname.upper() + suffix.lower(),
 		'UPPER'     :modname.upper() + suffix.upper()}[conf.env.FC_MOD_CAPITALIZATION or 'lower']
 
+@conf
+def is_fortran_mod(conf, node):
+	return str(node).lower().endswith('.mod')
+
 def get_fortran_tasks(tsk):
 	"""
 	Obtains all fortran tasks from the same build group. Those tasks must not have
@@ -142,7 +146,7 @@ class fc(Task.Task):
 				# ourselves as task.dep_nodes (additional input nodes)
 				tmp = []
 				for t in outs[k]:
-					tmp.extend(t.outputs)
+					tmp.extend(f for f in t.outputs if bld.is_fortran_mod(f))
 				a.dep_nodes.extend(tmp)
 				a.dep_nodes.sort(key=lambda x: x.abspath())
 
