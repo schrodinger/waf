395793361d430a482580dda59709aae5ef41f152
Rafaël Kooi <3961583-RA-Kooi@users.noreply.gitlab.com>
2024-11-29 20:15:29 +0100

    qt5: Add help text for qtdir and qtbin options

--- waflib/Tools/qt5.py
+++ waflib/Tools/qt5.py
@@ -1197,8 +1197,8 @@ def options(opt):
 	"""
 	opt.add_option('--want-rpath', action='store_true', default=False, dest='want_rpath', help='enable the rpath for qt libraries')
 
-	for i in 'qtdir qtbin'.split():
-		opt.add_option('--'+i, type=str, default='', dest=i)
+	opt.add_option('--qtdir', type=str, default='', dest='qtdir', help='path to the root of the qt installation; to aid finding qmake')
+	opt.add_option('--qtbin', type=str, default='', dest='qtbin', help='path to the bin folder of the qt installation; to aid finding qmake')
 
 	opt.add_option('--translate', action='store_true', help='collect translation strings', dest='trans_qt5', default=False)
 	opt.add_option('--qtextralibs', type=str, default='', dest='qtextralibs', help='additional qt libraries on the system to add to default ones, comma separated')
